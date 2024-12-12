b4f7dbeafd7a1e21df353fd041d6cc7aaa18b928
Hossein <hosseinbehvandi80@gmail.com>
2024-12-13 02:16:33 +0330

    magic transform for make <AnimalShow/>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { useState } from "react";
-
+import AnimalShow from "./AnimalShow";
 function getRandomAnimal(){
     const animals = ['bird' , 'cat' , 'cow' , 'dog' , 'gator' , 'horse']
 
     return animals[Math.floor(Math.random() * animals.length)]
 }
 
+
 function App(){
     const [animals , setAnimals] = useState([])
     const handleClick = () => {
         setAnimals([...animals, getRandomAnimal()])
     }
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
     return(
         <div>
         <button onClick={handleClick}>Add Animal</button>
-        <p>Number of animals: {animals} </p>
+        <AnimalShow ></AnimalShow>
+        <p>{renderedAnimals} </p>
         </div>
     )
 }
